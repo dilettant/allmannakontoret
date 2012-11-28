5babdb455f08dcf974d9e1c43aa3f8bd9d0804cb
vectorsize <vectorsize@gmail.com>
2012-11-28 13:31:57 +0100

    fixing the cookie mechanismo for the newsletter

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,7 +71,8 @@ $(document).ready(function(){
     console.log('matchme', matchme);
     console.log('matched', matched);
     console.log('matched + matchme.length', matched + matchme.length);
-    
+    console.log('matched + matchme.length', matched + matchme.length);
+    console.log('-20,2', cookies[i].substr(-20,matchme.length));
     thepublicoffice = cookies[i].substr(matched, matched + matchme.length);
     thepublicofficeEmail = cookies[i].substr(0,cookies[i].indexOf("thepublicoffice-email="));
   };
